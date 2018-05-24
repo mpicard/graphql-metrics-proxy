e34749993d972dd9ce84b8a23ef6fa7436d994ea
Martin Picard <martin8768@gmail.com>
2018-05-24 13:25:30 +0100

    Fix origin http/s error

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,35 +6,33 @@ import * as request from 'request';
 const port = process.env.PORT || 4000;
 const app = express();
 
-let Agent;
-if (process.env.NODE_ENV === 'production') {
-  Agent = https.Agent;
-} else {
-  Agent = http.Agent;
-}
+const keepAliveAgent = () =>
+  process.env.NODE_ENV === "production"
+    ? new https.Agent({ keepAlive: true })
+    : new http.Agent({ keepAlive: true });
 
-// create default request to the original graphQL server
+// create default request to the original graphQL ser\ver
 const origin = request.defaults({
-  baseUrl: 'http://localhost:5000/',
-  agent: new Agent({ keepAlive: true })
+  baseUrl: "http://localhost:5000/",
+  agent: keepAliveAgent()
 });
 
 // create default request to api server
-const baseUrl = process.env.METRICS_API_URI || 'http://localhost:8000/api/';
 const api = request.defaults({
-  baseUrl, agent: new Agent({ keepAlive: true })
+  baseUrl: process.env.METRICS_API_URI || "http://localhost:8000/api/",
+  agent: keepAliveAgent()
 });
 
 // catch all /graphql requests with json body parser
-app.use('/graphql', express.json(), (req, res, next) => {
+app.use("/graphql", express.json(), (req, res, next) => {
   // we'll save the query string and (optional) operationName
   // build a similar request to the incoming request
   let query, operationName;
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     query = req.body.query;
     operationName = req.body.operationName;
-  } else if (req.method === 'GET') {
+  } else if (req.method === "GET") {
     query = req.query.query;
     operationName = req.query.operationName;
   }
@@ -65,15 +63,13 @@ app.use('/graphql', express.json(), (req, res, next) => {
 });
 
 // pipe all other requests (like graphiql) to original
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
   const options = { uri: req.path, qs: req.query };
-  req
-    .pipe(origin(options))
-    .pipe(res);
+  req.pipe(origin(options)).pipe(res);
 });
 
 app.listen(port, () => {
-  console.log('Proxy started http://localhost:4000/graphiql');
+  console.log("Proxy started http://localhost:4000/graphiql");
 });
 
 /**
@@ -86,18 +82,18 @@ app.listen(port, () => {
  */
 function collectMetrics(query, operationName, extensions, errors) {
   // clean up query to remove sensitive data from parameters
-  query = query.replace(/(\:.*?)(?=\,)|(\:.*?)(?=\))/g, '');
+  query = query.replace(/(\:.*?)(?=\,)|(\:.*?)(?=\))/g, "");
   // remove redundant whitespace
-  query = query.replace(/\s+/g, ' ').trim();
+  query = query.replace(/\s+/g, " ").trim();
 
   const options = {
-    url: '/metrics',
-    method: 'POST',
+    url: "/metrics",
+    method: "POST",
     json: { query, operationName, extensions, errors }
   };
   api(options, (err, res, body) => {
     if (err || res.statusCode !== 200) {
-      console.log('Proxy: POST error:', err.message || body);
+      console.log("Proxy: POST error:", err.message || body);
     }
   });
 }
